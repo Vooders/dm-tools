3650f4e6b2f766761c914c3f4577e00776facf3a
Kev Wilson <kev.wilson@bbc.co.uk>
2023-08-13 10:04:38 +0100

    Adds more helpful error page
    The error page includes instructions on how to fix the most
    common issue of stale character data after an update.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -113,69 +113,69 @@ function DashboardContent() {
         },
       })}
     >
-      <ErrorBoundary>
-        <HashRouter>
-          <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="absolute" open={open}>
-              <Toolbar
-                sx={{
-                  pr: '24px', // keep right padding when drawer closed
-                }}
-              >
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                  onClick={toggleDrawer}
-                  sx={{
-                    marginRight: '36px',
-                    ...(open && { display: 'none' }),
-                  }}
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Typography
-                  component="h1"
-                  variant="h6"
-                  color="inherit"
-                  noWrap
-                  sx={{ flexGrow: 1 }}
-                >
-                  DM Tools
-                </Typography>
-                <UpdateAllButton />
-              </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-              <Toolbar
-                sx={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  justifyContent: 'flex-end',
-                  px: [1],
-                }}
-              >
-                <IconButton onClick={toggleDrawer}>
-                  <ChevronLeftIcon />
-                </IconButton>
-              </Toolbar>
-              <Divider />
-              <MainMenu />
-            </Drawer>
-            <Box
-              component="main"
+      <HashRouter>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar position="absolute" open={open}>
+            <Toolbar
               sx={{
-                backgroundColor: (theme) =>
-                  theme.palette.mode === 'light'
-                    ? theme.palette.grey[100]
-                    : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '100vh',
-                overflow: 'auto',
+                pr: '24px', // keep right padding when drawer closed
               }}
             >
-              <Toolbar />
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                  marginRight: '36px',
+                  ...(open && { display: 'none' }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+              >
+                DM Tools
+              </Typography>
+              <UpdateAllButton />
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1],
+              }}
+            >
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <MainMenu />
+          </Drawer>
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: '100vh',
+              overflow: 'auto',
+            }}
+          >
+            <Toolbar />
+            <ErrorBoundary>
               <Container maxWidth='lg' sx={{ my: 4 }}>
                 <Grid container spacing={3} >
                   {/* Recent Inventory */}
@@ -197,10 +197,10 @@ function DashboardContent() {
                 </Grid>
                 <Copyright sx={{ pt: 4 }} />
               </Container>
-            </Box>
+            </ErrorBoundary>
           </Box>
-        </HashRouter>
-      </ErrorBoundary>
+        </Box>
+      </HashRouter>
     </ThemeProvider>
   );
 }
--- src/components/ErrorBoundry.tsx
+++ src/components/ErrorBoundry.tsx
@@ -1,12 +1,14 @@
+import { Typography } from '@mui/material';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import React from "react";
 import { Component } from "react";
- 
+
 class ErrorBoundary extends Component {
   constructor(props: any) {
     super(props);
     this.state = { hasError: false };
   }
- 
+
   static getDerivedStateFromError(error: any) {
     // Update state so the next render will show the fallback UI.
     return {
@@ -14,22 +16,45 @@ class ErrorBoundary extends Component {
       error,
     };
   }
- 
+
   componentDidCatch(error: any, errorInfo: any) {
     // You can also log the error to an error reporting service like AppSignal
     // logErrorToMyService(error, errorInfo);
   }
- 
+
   render() {
     const { hasError, error } = this.state as any;
- 
+
     if (hasError) {
+      const cantReadProperty = error.message && error.message.includes("Cannot read properties of undefined")
       // You can render any custom fallback UI
       return (
-        <div>
-          <p>Something went wrong 😭</p>
- 
-          {error.message && <span>Here's the error: {error.message}</span>}
+        <div >
+          <Typography variant='h3'>Something went wrong 😭</Typography>
+          {cantReadProperty &&
+            <>
+              <Typography variant='h6'>
+                It appears that the character data is out of date.
+              </Typography>
+              <Typography variant='subtitle1'>
+                Click the UPDATE button and restart the app
+              </Typography>
+            </>
+          }
+
+          {!cantReadProperty &&
+            <>
+              <Typography variant='h6'>
+                Something's proper broke
+              </Typography>
+              <Typography variant='subtitle1'>
+                Tell Kev or Ste and show them this
+              </Typography>
+              <Typography>
+                {error.message}
+              </Typography>
+            </>
+          }
         </div>
       );
     }
@@ -38,5 +63,5 @@ class ErrorBoundary extends Component {
     return this.props.children;
   }
 }
- 
+
 export default ErrorBoundary;
