34dd2ef30750172a6f36a5ba7150918ce9c362db
Steven Grindey <steven.grindey@gmail.com>
2023-08-14 22:31:25 +0100

    started styling wealth page

--- src/components/pages/Wealth.tsx
+++ src/components/pages/Wealth.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Title from '../Title'
-import { Typography, Card, CardContent } from '@mui/material'
+import { Typography, Card, CardContent, Grid } from '@mui/material'
 import { WealthData } from '../../handlers/getWealth'
 import Currencies from '../fragments/Currencies'
 import Currency from '../fragments/Currency'
@@ -25,30 +25,48 @@ export default function Wealth() {
         <React.Fragment>
             <Title>Wealth</Title>
 
-            {wealth.map(character => {
-                return (
-                    <Card variant="outlined">
-                        <CardContent>
-                            <Typography component="div" variant="h5">
-                                {character.name}
-                            </Typography>
-                            <Currencies align='left' currencies={character.currencies} />
-                            Currencies Total:
-                            <Currency size='large' amount={character.currencies.total} icon='gold' />
-                            {character.containers.map(container => {
-                                return <>
-                                    <Typography >
-                                        {container.name}
+            <Grid
+                container
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="flex-start"
+                spacing={1}
+            >
+                {wealth.map(character => {
+                    return (
+                        <Grid item >
+                            <Card variant="outlined" >
+                                <CardContent>
+                                    <Typography component="div" variant="h5" >
+                                        {character.name}
                                     </Typography>
-                                    <Currency amount={container.value} icon='gold' />
-                                </>
-                            })}
-                            Containers Total:
-                            <Currency size="large" amount={character.totalContainerWealth} icon='gold' />
-                        </CardContent>
-                    </Card>
-                )
-            })}
+                                    <Currencies align='left' currencies={character.currencies} />
+                                    <Grid item display="flex">
+                                        <Typography component="div" variant="h6" >
+                                            Total Currency:
+                                        </Typography>
+                                        <Currency size='large' amount={character.currencies.total} icon='gold' />
+                                    </Grid>
+                                    {character.containers.map(container => {
+                                        return <Grid item display="flex" >
+                                            <Typography >
+                                                {container.name}:
+                                            </Typography>
+                                            <Currency amount={container.value} icon='gold' />
+                                        </Grid>
+                                    })}
+                                    <Grid item display="flex">
+                                        <Typography component="div" variant="h6">
+                                            Containers Total:
+                                        </Typography>
+                                        <Currency size="large" amount={character.totalContainerWealth} icon='gold' />
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    )
+                })}
+            </Grid>
         </React.Fragment>
     )
 
