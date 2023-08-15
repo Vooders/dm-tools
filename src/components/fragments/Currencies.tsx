dc09380d6931faacd7464f3e4b008ae475a6f397
polynomiald <steven.grindey@gmail.com>
2023-08-15 21:55:45 +0100

    renamed a function in currencies

--- src/components/fragments/Currencies.tsx
+++ src/components/fragments/Currencies.tsx
@@ -8,7 +8,7 @@ export default function Currencies(props: CurrenciesProps) {
   const currencyAmount = props.currencies
   const direction = props.align === 'left' ? 'row' : 'row-reverse'
 
-  const displayZeroes = (amount: number) => {
+  const displayCurrency = (amount: number) => {
     return amount > 0 || props.showZeroes
   }
 
@@ -16,27 +16,27 @@ export default function Currencies(props: CurrenciesProps) {
     <>
       <Grid container padding={0} columnSpacing={1} direction={direction} justifyContent="flex-start" >
 
-      {displayZeroes(currencyAmount.cp) &&
+      {displayCurrency(currencyAmount.cp) &&
           <Grid item>
             <Currency amount={currencyAmount.cp} icon='copper'/>
           </Grid>
         }
-        {displayZeroes(currencyAmount.sp) &&         
+        {displayCurrency(currencyAmount.sp) &&         
          <Grid item>
             <Currency amount={currencyAmount.sp} icon='silver'/>
           </Grid>
         }
-        {displayZeroes(currencyAmount.ep) &&      
+        {displayCurrency(currencyAmount.ep) &&      
          <Grid item>
             <Currency amount={currencyAmount.ep} icon='electrum'/>
           </Grid>
         }
-        {displayZeroes(currencyAmount.gp) &&       
+        {displayCurrency(currencyAmount.gp) &&       
          <Grid item>
             <Currency amount={currencyAmount.gp} icon='gold'/>
           </Grid>
         }
-        {displayZeroes(currencyAmount.pp) &&         
+        {displayCurrency(currencyAmount.pp) &&         
         <Grid item>
             <Currency amount={currencyAmount.pp} icon='platinum'/>
           </Grid>
