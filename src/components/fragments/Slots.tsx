fb486329966b3cf88b43fc11e8948532194ae5f0
Steven Grindey <steven.grindey@gmail.com>
2023-09-29 00:01:51 +0100

    makes a custom box component for slots
    changed shadows and styling and put it into its own component

--- src/components/fragments/Slots.tsx
+++ src/components/fragments/Slots.tsx
@@ -18,50 +18,50 @@ export default function Slots(props: SlotsProps) {
         }
     })
 
+    const SlotsBox = styled(Box)({
+        marginRight: 20,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingTop: 4,
+        paddingBottom: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8
+    })
+
     const boxStyle = () => {
         if (props.max - props.used != 0 && props.highlight) {
             return {
                 color: 'rgb(220,220,220)',
-                boxShadow: 5,
+                boxShadow: '0 3px 5px 2px rgba(135, 135, 135, .2)',
                 background: 'linear-Gradient(rgb(10,50,90), rgb(30,70,110))',
             }
         }
         return {
-            color: 'rgb(190, 190, 190)',
-            boxShadow: 1        
+            color: 'rgb(180, 180, 180)',
+            boxShadow: '0 1px 3px 1px rgba(135, 135, 135, .1)'        
         }
     }
 
-    const iconColor = (props.highlight) ? { color: 'white'} : {color: 'gray'}
+    const iconColor = (props.highlight) ? { color: 'white' } : { color: 'gray' }
 
     return (
-        <Box mr={3}
-            px={1}
-            py={0.5}
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='center'
-            border={1}
-            borderRadius={2}
-            borderColor='rgb(10, 35, 57)'
-            sx={boxStyle}
-        >
+        <SlotsBox sx={boxStyle}>
             <Typography component="legend" fontSize={17}> {props.title}</Typography>
             {props.max < 10 ?
                 <StyledRating
-                    name="customized-color"
                     defaultValue={props.max - props.used}
                     max={props.max}
                     readOnly
-                    precision={0.5}
                     icon={<Brightness1Icon fontSize="inherit" sx={iconColor} />}
                     emptyIcon={<BlockIcon fontSize="inherit" />}
                 />
                 :
                 <Typography variant='h6'>{`${props.max - props.used} / ${props.max}`}</Typography>
-            }             
-        </Box>
+            }
+        </SlotsBox>
     )
 }
 
