3f9d99e5c6c7a51b3b4d234ff1563f249c428f90
Steven Grindey <steven.grindey@gmail.com>
2023-10-01 02:04:45 +0100

    adds unnarmored ac calculations for barbarian and monk

--- src/lib/character-sheet-processor/armourClass.ts
+++ src/lib/character-sheet-processor/armourClass.ts
@@ -1,10 +1,17 @@
-import { Ability } from "../CharacterSheetProcessor"
+import { Ability, CharacterProfile } from "../CharacterSheetProcessor"
 
 const baseAC = 10
 
-export default function armourClass(abilities: Ability[]): number {
+export default function armourClass(abilities: Ability[], profile: CharacterProfile): number {
     const dexMod = getModifier('DEX', abilities)
-    return baseAC + dexMod
+
+    if (profile.classes === 'Barbarian') {
+        return baseAC + dexMod + getModifier('CON', abilities)
+    } else if (profile.classes === 'Monk') {
+        return baseAC + dexMod + getModifier('WIS', abilities)
+    } else {
+        return baseAC + dexMod
+    }
 }
 
 function getModifier(shortName: string, abilities: Ability[]) {
