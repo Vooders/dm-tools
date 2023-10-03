c748758cddbbdeba6c1d4148f7b6a293a8251c16
Steven Grindey <steven.grindey@gmail.com>
2023-10-03 17:37:54 +0100

    adds ac from items
    added a function that adds up the ac from all the equipped items of subType armor

--- src/lib/character-sheet-processor/armourClass.ts
+++ src/lib/character-sheet-processor/armourClass.ts
@@ -1,12 +1,23 @@
-import { Modifiers, Ability } from "../CharacterSheetProcessor"
+import { Modifiers, Ability, ItemContainer } from "../CharacterSheetProcessor"
 
 const baseAC = 10
 
-export default function armourClass(abilities: Ability[], modifiers: Modifiers): number {
-    const dexMod = getAbilityModifier('DEX', abilities)
+export default function armourClass(abilities: Ability[], modifiers: Modifiers, inventory: ItemContainer[]): number {
+    const dexModifier = getAbilityModifier('DEX', abilities)
     const unarmoredModifier = getUnarmoredModifier(abilities, modifiers)
+    const armoredAc = getAcFromItems(inventory)
 
-    return baseAC + dexMod + unarmoredModifier
+    if(armoredAc) {
+        return armoredAc
+    } else {
+        return baseAC + dexModifier + unarmoredModifier
+    }
+}
+
+function getAcFromItems(inventory: ItemContainer[]) {
+    return inventory.map((container) => container.contents).flat()
+        .filter((item) => item.equipped && item.definition.filterType === 'Armor')
+        .reduce((total: number, item: any) =>  total + item.definition.armorClass, 0)
 }
 
 function getUnarmoredModifier(abilities: Ability[], modifiers: Modifiers): number {
