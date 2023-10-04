70122fcf09feaf4c3f4dcc74ecd7d2acdf0ecc5d
Steven Grindey <steven.grindey@gmail.com>
2023-10-05 00:35:51 +0100

    adds armored modifier
    added a function to get the modifier for wearing armor
    
    renamed another function

--- src/lib/character-sheet-processor/armourClass.ts
+++ src/lib/character-sheet-processor/armourClass.ts
@@ -5,25 +5,30 @@ const baseAC = 10
 export default function armourClass(abilities: Ability[], modifiers: Modifiers, inventory: ItemContainer[]): number {
     const dexModifier = getAbilityModifier('DEX', abilities)
     const unarmoredModifier = getUnarmoredModifier(abilities, modifiers)
+    const armoredModifier = getArmoredModifier(modifiers)
     const equippedArmorItems = getEquippedArmorItems(inventory)
     const equippedArmorItemsAc = getEquippedArmorItemsAc(equippedArmorItems)
+    const armorTypeModifier = getArmorTypeModifier(equippedArmorItems, dexModifier)
 
     if (equippedArmorItemsAc > 10) {
-        const armoredModifier = getArmoredModifier(equippedArmorItems, dexModifier)
-        return equippedArmorItemsAc + armoredModifier
+        return equippedArmorItemsAc + armorTypeModifier + armoredModifier
     } else {
         return baseAC + dexModifier + unarmoredModifier + equippedArmorItemsAc
     }
 }
 
-function getArmoredModifier(items: Item[], dexModifier: number) {
-    const armorType = items.filter((item) => item.definition.armorTypeId === 1 || 2 || 3)[0].definition.armorTypeId
+function getArmorTypeModifier(items: Item[], dexModifier: number) {
+    const armorType = getArmorType(items)
 
     if (armorType === 1) {
         return dexModifier
     } else if (armorType === 2) {
         return dexModifier < 2 ? dexModifier : 2
     } else return 0
+}
+
+function getArmorType(items: Item[]) {
+    return items.length > 0 ? items.filter((item) => item.definition.armorTypeId === 1 || 2 || 3)[0].definition.armorTypeId : 0
 }
 
 function getEquippedArmorItems(inventory: ItemContainer[]) {
@@ -37,6 +42,11 @@ function getEquippedArmorItemsAc(items: Item[]): number {
 function getUnarmoredModifier(abilities: Ability[], modifiers: Modifiers): number {
     const modifier = modifiers.class.filter((modifier) => modifier.subType === 'unarmored-armor-class')[0]
     return modifier ? abilities[modifier.statId - 1].modifier : 0
+}
+
+function getArmoredModifier(modifiers: Modifiers): number {
+    const modifier = modifiers.class.filter((modifier) => modifier.subType === 'armored-armor-class')[0]
+    return modifier ? modifier.fixedValue : 0
 }
 
 function getAbilityModifier(shortName: string, abilities: Ability[]) {
