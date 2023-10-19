898699e46802a7785c64befa67971ab755f85530
Kev Wilson <kev.wilson@bbc.co.uk>
2023-10-19 18:01:02 +0100

    Adds tests for the abilities class
    I commented out a lot of tests yesterday. I've rewrote them and
    added some others which were missing.

--- test/lib/character-sheet-processor/abilities.spec.ts
+++ test/lib/character-sheet-processor/abilities.spec.ts
@@ -1,70 +1,129 @@
+import ModifierBuilder from '../../../test/builders/ModifierBuilder';
 import abilities from '../../../src/lib/character-sheet-processor/abilities'
 
-const stats = buildStats()
-
 describe('abilities', () => {
-    // it('should give each ability a name', () => {
-    //     const result = abilities(stats, modifiers)[0].name
-    //     const result2 = abilities(stats, modifiers)[2].name
-    //     const result3 = abilities(stats, modifiers)[4].name
-    //     result.should.equal('Strength')
-    //     result2.should.equal('Constitution')
-    //     result3.should.equal('Wisdom')
-    // })
+    describe('names', () => {
+        const nameTests = [
+            {args: 0, expected: 'Strength'},
+            {args: 1, expected: 'Dexterity'},
+            {args: 2, expected: 'Constitution'},
+            {args: 3, expected: 'Intelligence'},
+            {args: 4, expected: 'Wisdom'},
+            {args: 5, expected: 'Charisma'}
+        ];
 
-    // it('should give each ability a value', () => {
-    //     const stats = buildStats(0, 20, 0, 15, 0, 10)
-    //     const result = abilities(stats, modifiers)[1].value
-    //     const result2 = abilities(stats, modifiers)[3].value
-    //     const result3 = abilities(stats, modifiers)[5].value
-    //     result.should.equal(20)
-    //     result2.should.equal(15)
-    //     result3.should.equal(10)
-    // })
+        nameTests.forEach((test) => {
+            it(`Ability ${test.args + 1}'s name should be ${test.expected}`, function() {
+                const stats = buildStats(10, 10, 10, 10, 10, 10)
 
-    // it('should give each ability a short name', () => {
-    //     const result = abilities(stats, modifiers)[0].shortName
-    //     const result2 = abilities(stats, modifiers)[3].shortName
-    //     const result3 = abilities(stats, modifiers)[4].shortName
-    //     result.should.equal('STR')
-    //     result2.should.equal('INT')
-    //     result3.should.equal('WIS')
-    // })
+                const theAbilities = abilities(stats, [])
 
-    // it('should add modifiers', () => {
-    //     const modifiers = buildModifiers(null, null, 2, 2)
-    //     const modifiers2 = buildModifiers(null, null, 1, 4)
-    //     const modifiers3 = buildModifiers(null, null, 3, 6, null, null, 5, 1)
-    //     const result = abilities(stats, modifiers)[1].value
-    //     const result2 = abilities(stats, modifiers2)[3].value
-    //     const result3 = abilities(stats, modifiers3)[0].value
-    //     result.should.equal(12)
-    //     result2.should.equal(11)
-    //     result3.should.equal(15)
-    // })
+                const name = theAbilities[test.args].name
+                name.should.equal(test.expected)
+            })
+        })
+    })
+
+    describe('short names', () => {
+        const shotNameTests = [
+            {args: 0, expected: 'STR'},
+            {args: 1, expected: 'DEX'},
+            {args: 2, expected: 'CON'},
+            {args: 3, expected: 'INT'},
+            {args: 4, expected: 'WIS'},
+            {args: 5, expected: 'CHA'}
+        ];
+
+        shotNameTests.forEach((test) => {
+            it(`Ability ${test.args + 1}'s short name should be ${test.expected}`, function() {
+                const stats = buildStats(10, 10, 10, 10, 10, 10)
+
+                const theAbilities = abilities(stats, [])
+
+                const shortName = theAbilities[test.args].shortName
+                shortName.should.equal(test.expected)
+            })
+        })
+    })
+
+    describe('modifiers', () => {
+        const modifiersTests = [
+            {args: 1, expected: -5},
+            {args: 2, expected: -4},
+            {args: 3, expected: -4},
+            {args: 4, expected: -3},
+            {args: 5, expected: -3},
+            {args: 6, expected: -2},
+            {args: 7, expected: -2},
+            {args: 8, expected: -1},
+            {args: 9, expected: -1},
+            {args: 10, expected: 0},
+            {args: 11, expected: 0},
+            {args: 12, expected: 1},
+            {args: 13, expected: 1},
+            {args: 14, expected: 2},
+            {args: 15, expected: 2},
+            {args: 16, expected: 3},
+            {args: 17, expected: 3},
+            {args: 18, expected: 4},
+            {args: 19, expected: 4},
+            {args: 20, expected: 5},
+            {args: 21, expected: 5},
+            {args: 22, expected: 6},
+            {args: 23, expected: 6},
+            {args: 24, expected: 7},
+            {args: 25, expected: 7},
+            {args: 26, expected: 8},
+            {args: 27, expected: 8},
+            {args: 28, expected: 9},
+            {args: 29, expected: 9},
+            {args: 30, expected: 10}
+        ];
+
+        modifiersTests.forEach((test) => {
+            it(`a stat of ${test.args} should have a modifier of ${test.expected}`, function() {
+                const stats = buildStats(test.args, 10, 10, 10, 10, 10)
+
+                const theAbilities = abilities(stats, [])
+
+                theAbilities[0].modifier.should.equal(test.expected)
+            })
+        })
+    })
+
+    describe('bonus modifiers', () => {
+        it('should increase a stats value', () => {
+            const stats = buildStats(10, 10, 10, 10, 10, 10)
+            const plusTwoDex = new ModifierBuilder()
+                .withEntityId(2)
+                .withFixedValue(2)
+                .build()
+
+            const theAbilities = abilities(stats, [plusTwoDex])
+
+            theAbilities[1].value.should.equal(12)
+        })
+
+        it('should increase a stats modifier', () => {
+            const stats = buildStats(10, 10, 10, 10, 10, 10)
+            const plusTwoDex = new ModifierBuilder()
+                .withEntityId(2)
+                .withFixedValue(2)
+                .build()
+
+            const theAbilities = abilities(stats, [plusTwoDex])
+
+            theAbilities[1].modifier.should.equal(1)
+        })
+    })
 })
 
-function buildStat(id: number = null, name: string = null, value: number = null) {
-    return {
-        id,
-        name,
-        value
-    }
-}
-
-function buildStats(
-    value1: number = 10,
-    value2: number = 10,
-    value3: number = 10,
-    value4: number = 10,
-    value5: number = 10,
-    value6: number = 10) {
-    return [
-        buildStat(1, null, value1),
-        buildStat(2, null, value2),
-        buildStat(3, null, value3),
-        buildStat(4, null, value4),
-        buildStat(5, null, value5),
-        buildStat(6, null, value6)
-    ]
+function buildStats(...args: [number, number, number, number, number, number]) {
+    return args.map((value: number, index: number) => {
+        return {
+            id: index+1,
+            name: "someName",
+            value
+        }
+    })
 }
