4c374113313fb822a930cbf4c32589f5497cb808
Steven Grindey <steven.grindey@gmail.com>
2023-10-20 03:37:25 +0100

    removes buildContainers and reduceAndRound
    I have moved the functions into their own file so now getWealth just gets its data from DmToolsData and the new wealth file will put the data there

--- src/handlers/getWealth.ts
+++ src/handlers/getWealth.ts
@@ -1,40 +1,20 @@
 import getSummaryData from '../lib/getSummary'
 import getCharacter from './getCharacter'
-import { Currencies, ItemContainer } from '../lib/CharacterSheetProcessor'
-import { Item } from '../lib/CharacterSheetProcessor'
+import { ContainerWealth, Currencies } from '../lib/CharacterSheetProcessor'
 
 export default async (): Promise<WealthData[]> => {
     const summary = await getSummaryData()
     const characterIds = Object.keys(summary)
 
-    function reduceAndRound<T>(someArray: T[], reduceFunc: (acc: number, item: T) => number): number {
-        const result = someArray.reduce(reduceFunc, 0)
-        return Math.round(result * 100) / 100
-    }
-
-    function buildContainers(inventory: ItemContainer[]): Container[] {
-        return inventory.map((inventory) => {
-            return {
-                name: inventory.name,
-                value: reduceAndRound<Item>(inventory.contents, (total, item) => {
-                    return total + (item.definition.cost / item.definition.bundleSize) * item.quantity
-                })
-            }
-        })
-    }
-
     return await Promise.all(characterIds.map(async (characterId) => {
         const characterData = await getCharacter(null, characterId)
-        const containers = buildContainers(characterData.inventory)
-        const totalContainerWealth = reduceAndRound<Container>(containers, (total, container) => total + container.value)
-        const totalWealth = Math.round(totalContainerWealth + characterData.currencies.total * 100) / 100
-
+        
         return {
             name: characterData.profile.name,
             currencies: characterData.currencies,
-            containers,
-            totalContainerWealth,
-            totalWealth
+            containers: characterData.wealth.containers,
+            totalContainerWealth: characterData.wealth.totalContainerWealth,
+            totalWealth: characterData.wealth.totalWealth
         }
     }))
 }
@@ -42,12 +22,7 @@ export default async (): Promise<WealthData[]> => {
 export type WealthData = {
     name: string
     currencies: Currencies
-    containers: Container[]
+    containers: ContainerWealth[]
     totalContainerWealth: number
     totalWealth: number
-}
-
-export type Container = {
-    name: string
-    value: number
 }
