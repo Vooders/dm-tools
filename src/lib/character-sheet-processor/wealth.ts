1ca0fb4533eccd6a0eabc20eb33162cf63fe2fbd
Steven Grindey <steven.grindey@gmail.com>
2023-10-20 03:44:23 +0100

    adds a new wealth file

---
+++ src/lib/character-sheet-processor/wealth.ts
@@ -0,0 +1,36 @@
+import { ContainerWealth, Currencies, Item, ItemContainer } from "../CharacterSheetProcessor"
+
+export default function wealth(inventory: ItemContainer[], currencies: Currencies): Wealth {
+
+    function reduceAndRound<T>(someArray: T[], reduceFunc: (acc: number, item: T) => number): number {
+        const result = someArray.reduce(reduceFunc, 0)
+        return Math.round(result * 100) / 100
+    }
+
+    function buildContainers(itemContainers: ItemContainer[]): ContainerWealth[] {
+        return itemContainers.map((inventory) => {
+            return {
+                name: inventory.name,
+                value: reduceAndRound<Item>(inventory.contents, (total, item) => {
+                    return total + (item.definition.cost / item.definition.bundleSize) * item.quantity
+                })
+            }
+        })
+    }
+
+    const containers = buildContainers(inventory)
+    const totalContainerWealth = reduceAndRound<ContainerWealth>(containers, (total, container) => total + container.value)
+    const totalWealth = Math.round(totalContainerWealth + (currencies.total * 100) / 100)
+
+    return {
+        containers,
+        totalContainerWealth,
+        totalWealth
+    }
+}
+
+type Wealth = {
+    containers: ContainerWealth[]
+    totalContainerWealth: number
+    totalWealth: number
+}
