3893a831ce68d3f0f13e03e011897648a30cc7a7
Steven Grindey <steven.grindey@gmail.com>
2024-02-09 02:18:59 +0000

    adds action builder

---
+++ test/builders/ActionBuilder.ts
@@ -0,0 +1,54 @@
+
+export default class ActionBuilder {
+    constructor(
+        private name: string = '',
+        private description: string = '',
+        private snippet: string = '',
+        private maxUses: number = 0,
+        private numberUsed: number = 0,
+        private useProficiencyBonus: boolean = true
+    ) { }
+
+    public withName(name: string) {
+        this.name = name
+        return this
+    }
+
+    public withDescription(description: string) {
+        this.description = description
+        return this
+    }
+
+    public withSnippet(snippet: string) {
+        this.snippet = snippet
+        return this
+    }
+
+    public withMaxUses(maxUses: number) {
+        this.maxUses = maxUses
+        return this
+    }
+
+    public withNumberUsed(numberUsed: number) {
+        this.numberUsed = numberUsed
+        return this
+    }
+
+    public withUseProficiencyBonus(useProficiencyBonus: boolean) {
+        this.useProficiencyBonus = useProficiencyBonus
+        return this
+    }
+
+    public build() {
+        return {
+            name: this.name,
+            description: this.description,
+            snippet: this.snippet,
+            limitedUse: {
+                maxUses: this.maxUses,
+                numberUsed: this.numberUsed,
+                useProficiencyBonus: this.useProficiencyBonus
+            }
+        }
+    }
+}
