87ef4a0db17650f281c46b477b0d85b9b912f831
Steven Grindey <steven.grindey@gmail.com>
2024-02-09 17:11:44 +0000

    adds tests for feats

--- test/lib/character-sheet-processor/actions.spec.ts
+++ test/lib/character-sheet-processor/actions.spec.ts
@@ -1,49 +1,91 @@
 import ActionBuilder from "../../../test/builders/ActionBuilder";
 import ActionsBuilder from "../../../test/builders/ActionsBuilder";
 import actions from '../../../src/lib/character-sheet-processor/actions'
+import FeatBuilder from "../../../test/builders/FeatBuilder";
 
-describe('actions', () => {
-    describe('maxUses', () => {
-        it('should use proficiency bonus if race useProficiencyBonus is true', () => {
+describe('Actions', () => {
+    describe('with an actions array', () => {
+        it('should get the correct maxUses', () => {
             const race = new ActionBuilder()
-                .withUseProficiencyBonus(true)
+                .withMaxUses(8)
                 .build()
 
-            const Actions = new ActionsBuilder()
+            const actionsArray = new ActionsBuilder()
                 .withRace([race])
                 .build()
 
-            const maxUses = actions(Actions, [], [], 3)[0].limitedUse.maxUses
+            const maxUses = actions(actionsArray, [], [], 3)[0].limitedUse.maxUses
 
-            maxUses.should.equal(3)
+            maxUses.should.equal(8)
         })
 
-        it('should use proficiency bonus if class useProficiencyBonus is true', () => {
+        it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
             const clas = new ActionBuilder()
                 .withUseProficiencyBonus(true)
                 .build()
 
-            const Actions = new ActionsBuilder()
+            const actionsArray = new ActionsBuilder()
                 .withClass([clas])
                 .build()
 
-            const maxUses = actions(Actions, [], [], 5)[0].limitedUse.maxUses
+            const maxUses = actions(actionsArray, [], [], 5)[0].limitedUse.maxUses
 
             maxUses.should.equal(5)
         })
 
-        it('should use proficiency bonus if feat useProficiencyBonus is true', () => {
+        it('should get the correct numberUsed', () => {
             const feat = new ActionBuilder()
-                .withUseProficiencyBonus(true)
+                .withNumberUsed(10)
                 .build()
 
-            const Actions = new ActionsBuilder()
+            const actionsArray = new ActionsBuilder()
                 .withFeat([feat])
                 .build()
 
-            const maxUses = actions(Actions, [], [], 10)[0].limitedUse.maxUses
+            const numberUsed = actions(actionsArray, [], [], 10)[0].limitedUse.numberUsed
 
-            maxUses.should.equal(10)
+            numberUsed.should.equal(10)
+        })
+    })
+
+    describe('with a feats array', () => {
+        it('should get the correct maxUses', () => {
+            const actionsArray = new ActionsBuilder()
+                .build()
+
+            const feat = new FeatBuilder()
+                .withMaxUses(9)
+                .build()
+
+            const maxUses = actions(actionsArray, [feat], [], 3)[0].limitedUse.maxUses
+
+            maxUses.should.equal(9)
+        })
+
+        it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
+            const actionsArray = new ActionsBuilder()
+                .build()
+
+            const feat = new FeatBuilder()
+                .withUseProficiencyBonus(true)
+                .build()
+
+            const maxUses = actions(actionsArray, [feat], [], 6)[0].limitedUse.maxUses
+
+            maxUses.should.equal(6)
+        })
+
+        it('should get the correct numberUsed', () => {
+            const actionsArray = new ActionsBuilder()
+                .build()
+
+            const feat = new FeatBuilder()
+                .withNumberUsed(2)
+                .build()
+
+            const maxUses = actions(actionsArray, [feat], [], 3)[0].limitedUse.numberUsed
+
+            maxUses.should.equal(2)
         })
     })
 })
