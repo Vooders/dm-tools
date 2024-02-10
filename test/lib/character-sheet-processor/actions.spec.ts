11e58a7a8590e4284a1c8dc7227e309d24688abf
Steven Grindey <steven.grindey@gmail.com>
2024-02-10 19:07:29 +0000

    adds item tests

--- test/lib/character-sheet-processor/actions.spec.ts
+++ test/lib/character-sheet-processor/actions.spec.ts
@@ -1,11 +1,12 @@
 import ActionBuilder from "../../../test/builders/ActionBuilder";
 import ActionsBuilder from "../../../test/builders/ActionsBuilder";
 import actions from '../../../src/lib/character-sheet-processor/actions'
-import FeatBuilder from "../../../test/builders/FeatBuilder";
+import SpellFeatBuilder from "../../../test/builders/SpellFeatBuilder";
+import ItemBuilder from "../../../test/builders/ItemBuilder";
 
 describe('Actions', () => {
     describe('with an actions array', () => {
-        it('should get the correct maxUses', () => {
+        it('should return the correct maxUses', () => {
             const race = new ActionBuilder()
                 .withMaxUses(8)
                 .build()
@@ -17,6 +18,20 @@ describe('Actions', () => {
             const maxUses = actions(actionsArray, [], [], 3)[0].limitedUse.maxUses
 
             maxUses.should.equal(8)
+        })
+
+        it('should return the correct numberUsed', () => {
+            const feat = new ActionBuilder()
+                .withNumberUsed(10)
+                .build()
+
+            const actionsArray = new ActionsBuilder()
+                .withFeat([feat])
+                .build()
+
+            const numberUsed = actions(actionsArray, [], [], 10)[0].limitedUse.numberUsed
+
+            numberUsed.should.equal(10)
         })
 
         it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
@@ -32,60 +47,91 @@ describe('Actions', () => {
 
             maxUses.should.equal(5)
         })
-
-        it('should get the correct numberUsed', () => {
-            const feat = new ActionBuilder()
-                .withNumberUsed(10)
-                .build()
-
-            const actionsArray = new ActionsBuilder()
-                .withFeat([feat])
-                .build()
-
-            const numberUsed = actions(actionsArray, [], [], 10)[0].limitedUse.numberUsed
-
-            numberUsed.should.equal(10)
-        })
     })
 
-    describe('with a feats array', () => {
-        it('should get the correct maxUses', () => {
-            const actionsArray = new ActionsBuilder()
+    describe('with a spell feats array', () => {
+        it('should return the correct maxUses', () => {
+            const emptyActionsArray = new ActionsBuilder()
                 .build()
 
-            const feat = new FeatBuilder()
+            const feat = new SpellFeatBuilder()
                 .withMaxUses(9)
                 .build()
 
-            const maxUses = actions(actionsArray, [feat], [], 3)[0].limitedUse.maxUses
+            const maxUses = actions(emptyActionsArray, [feat], [], 3)[0].limitedUse.maxUses
 
             maxUses.should.equal(9)
         })
 
-        it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
-            const actionsArray = new ActionsBuilder()
+        it('should return the correct numberUsed', () => {
+            const emptyActionsArray = new ActionsBuilder()
                 .build()
 
-            const feat = new FeatBuilder()
+            const feat = new SpellFeatBuilder()
+                .withNumberUsed(2)
+                .build()
+
+            const maxUses = actions(emptyActionsArray, [feat], [], 3)[0].limitedUse.numberUsed
+
+            maxUses.should.equal(2)
+        })
+
+        it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
+            const emptyActionsArray = new ActionsBuilder()
+                .build()
+
+            const feat = new SpellFeatBuilder()
                 .withUseProficiencyBonus(true)
                 .build()
 
-            const maxUses = actions(actionsArray, [feat], [], 6)[0].limitedUse.maxUses
+            const maxUses = actions(emptyActionsArray, [feat], [], 6)[0].limitedUse.maxUses
+
+            maxUses.should.equal(6)
+        })
+    })
+
+    describe('with an item array', () => {
+        it('should return the correct maxUses', () => {
+            const emptyActionsArray = new ActionsBuilder()
+                .build()
+
+            const item = new ItemBuilder()
+                .withCanEquip(true)
+                .withMaxUses(4)
+                .build()
+
+            const maxUses = actions(emptyActionsArray, [], [item], 3)[0].limitedUse.maxUses
+
+            maxUses.should.equal(4)
+        })
+
+        it('should return the correct numberUsed', () => {
+            const emptyActionsArray = new ActionsBuilder()
+                .build()
+
+            const item = new ItemBuilder()
+                .withCanEquip(true)
+                .withNumberUsed(2)
+                .build()
+
+            const maxUses = actions(emptyActionsArray, [], [item], 3)[0].limitedUse.numberUsed
+
+            maxUses.should.equal(2)
+        })
+
+        it('should use proficiency bonus for maxUses if useProficiencyBonus is true', () => {
+            const emptyActionsArray = new ActionsBuilder()
+                .build()
+
+            const item = new ItemBuilder()
+                .withCanEquip(true)
+                .withUseProficiencyBonus(true)
+                .build()
+
+            const maxUses = actions(emptyActionsArray, [], [item], 6)[0].limitedUse.maxUses
 
             maxUses.should.equal(6)
         })
 
-        it('should get the correct numberUsed', () => {
-            const actionsArray = new ActionsBuilder()
-                .build()
-
-            const feat = new FeatBuilder()
-                .withNumberUsed(2)
-                .build()
-
-            const maxUses = actions(actionsArray, [feat], [], 3)[0].limitedUse.numberUsed
-
-            maxUses.should.equal(2)
-        })
     })
 })
