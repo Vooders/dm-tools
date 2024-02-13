8b417534016d9aefef2506e80ade09511380fc38
Steven Grindey <steven.grindey@gmail.com>
2024-02-13 20:22:42 +0000

    changes the name of the potionAmount function to calculateAmount

--- src/lib/character-sheet-processor/healthPotions.ts
+++ src/lib/character-sheet-processor/healthPotions.ts
@@ -2,13 +2,13 @@ import { HealthPotions } from "../CharacterSheetProcessor"
 
 export default function healthPotions(inventory: any): HealthPotions {
 
-    const potionAmount = (potionName: string) => {
+    const calculateAmount = (potionName: string) => {
         return inventory.filter((item: any) => item.definition.name === potionName)
             .reduce((acc: any, item: any) => acc + item.quantity, 0)
     }
 
     return {
-        normal: potionAmount('Potion of Healing'),
-        greater: potionAmount('Potion of Healing (Greater)')
+        normal: calculateAmount('Potion of Healing'),
+        greater: calculateAmount('Potion of Healing (Greater)')
     }
 }
