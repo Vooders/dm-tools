9aa89af002501546393619831720946514a2f7af
Vooders <kev.wilson84@gmail.com>
2024-02-21 12:17:55 +0000

    Pulls out the getting of a file from getSummary
    I want to play with creating graphs of thing like hp. In order
    to do this we must store hp over time. I'm planning to create
    a file which stores metrics for the party. This file will need
    to be read and written each time we load the party.
    
    I've split out the getting of a file from disk into getFile, which
    given a path will get the file and return it as json. If the file
    does not exist, it will be created as an empty json object.

---
+++ src/lib/getFile.ts
@@ -0,0 +1,14 @@
+import { readFile, open } from 'fs/promises';
+
+export default async (path: string) => {
+    // Create file if it does not exist
+    const fileHandle = await open(path, 'a+')
+    await fileHandle.close()
+
+    const fileBuffer = await readFile(path)
+    let file = fileBuffer.toString()
+    if (file === '') {
+        file = '{}'
+    }
+    return JSON.parse(file)
+}
--- src/lib/getSummary.ts
+++ src/lib/getSummary.ts
@@ -1,22 +1,11 @@
 import { app } from 'electron';
-import { readFile, open } from 'fs/promises';
 import path from 'path'
+import getFile from './getFile'
 
 const directory = 'characters'
 const userDataPath = app.getPath('userData');
 const summaryPath = path.join(userDataPath, directory, 'summary.json');
 
 export default async () => {
-    // Create summary.json if it does not exist
-
-    console.log(userDataPath)
-    const fileHandle = await open(summaryPath, 'a+')
-    await fileHandle.close()
-
-    const fileBuffer = await readFile(summaryPath)
-    let file = fileBuffer.toString()
-    if (file === '') {
-        file = '{}'
-    }
-    return JSON.parse(file)
+    return await getFile(summaryPath)
 }
