773d7da7d829b4bf88fe9e67b9244f3015ca5620
polynomiald <steven.grindey@gmail.com>
2024-06-04 15:43:34 +0100

    removes npc component and moves the code into npcs

--- src/components/pages/Npcs.tsx
+++ src/components/pages/Npcs.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react"
-import { Npc } from '../../lib/saveNpc'
-import NpcComponent from "../fragments/Npc";
-import Title from "../Title";
+import Title from "../Title"
+import { DmToolsData } from "../../lib/CharacterSheetProcessor"
+import { Card, CardContent, Typography, IconButton, Tooltip } from "@mui/material"
+import { Box } from "@mui/system"
+import { Link } from "react-router-dom"
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined'
+import DeleteIcon from '@mui/icons-material/Delete'
 
 export default function Npcs() {
-    const [npcs, setNpcs] = useState<Npc[]>([])
+    const [npcs, setNpcs] = useState<DmToolsData[]>([])
 
     const setNpcData = async () => {
         console.log('getting Npcs')
@@ -21,19 +26,56 @@ export default function Npcs() {
         })
     }, [])
 
+    const handleDelete = async (id: string) => {
+        const result = await window.electron.deleteNpc(id)
+        console.log(`Delete ${id} - ${result}`)
+    }
+
+    const style = {
+        buttons: {
+            display: 'flex',
+            marginLeft: 'auto'
+        }
+    }
+
     return (
         <React.Fragment>
             <Title>Npcs</Title>
             {npcs.map(npc => {
                 return (
-                    <NpcComponent
-                        name={npc.name}
-                        race={npc.race}
-                        gender={npc.gender}
-                        notes={npc.notes}
-                        id={npc.id}
-                        abilities={npc.abilities}
-                    />
+                    <Card variant='outlined'>
+                        <Box display='flex' flexDirection='row'>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                    {npc.profile.name}
+                                </Typography>
+                                <Typography variant='h6' >
+                                    {npc.profile.race}
+                                </Typography>
+                            </CardContent>
+                            <Box sx={style.buttons}>
+                                <Link to={`/npcSheet/${npc.id}`}>
+                                    <Tooltip title="View">
+                                        <IconButton aria-label="view" >
+                                            <RemoveRedEyeIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Link>
+                                <Link to={`/createNpc/${npc.id}`}>
+                                    <Tooltip title="Edit">
+                                        <IconButton aria-label="edit" >
+                                            <EditOutlinedIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </Link>
+                                <Tooltip title="Delete">
+                                    <IconButton aria-label="delete" onClick={() => handleDelete(npc.id)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </Tooltip>
+                            </Box>
+                        </Box>
+                    </Card >
                 )
             })}
         </React.Fragment>
