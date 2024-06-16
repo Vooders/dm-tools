71f74fed4873d9871b96be5c7eb3a303d30e9fe9
Vooders <kev.wilson84@gmail.com>
2024-06-16 08:52:06 +0100

    Sends character:updated when new character is imported
    I removed this because I thought it was sending duplicate
    events. I don't think it was and it is needed to add the new
    character to the menu bar and character list in the add char
    page.

--- src/handlers/saveCharacter.ts
+++ src/handlers/saveCharacter.ts
@@ -4,6 +4,7 @@ import saveCharacter from "../lib/saveCharacter"
 export default (mainWindow: BrowserWindow) => {
     return async (_: Electron.IpcMainInvokeEvent, character: any): Promise<boolean> => {
         const char = await saveCharacter(character)
+        mainWindow.webContents.send('character:updated')
         return char
     }
 }
