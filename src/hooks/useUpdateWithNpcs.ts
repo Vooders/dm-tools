2c8a2af12e8330fda3727dd69fddd014eb24a3f7
Kev Wilson <kev.wilson@bbc.co.uk>
2024-06-17 16:53:22 +0100

    Adds custom hooks to re get data when NPMs or characters are updated
    Custom hooks for characters and NCPs
    * `handler` a string which is used to get data from the right handler, it sends an event to `${handler}:get`
    * `logId` Something to put infront of logs
    * `initialState` The first state of the variable, so things don't crash before the data is loaded
    * `data` An optional field if you want to pass data in the event, such as character ids etc
    
    They can fetch data of any type as long as the event is "SOMETHING:get"
    
    The characters hook will update when it gets a 'character:updated' event
    The NPCs will update on 'npc:updated'

---
+++ src/hooks/useUpdateWithNpcs.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+export default function useUpdateWithNpcs<T>(handler: string, logId: string, initialState: any, data?: any): T {
+    const [characters, setCharacters] = useState<T>(initialState)
+
+    useEffect(() => {
+        (async () => {
+            console.log(`${logId} Initial load of ${handler} data`)
+            setCharacters(await window.electron.invoke(`${handler}:get`, data))
+        })()
+
+        console.log(`${logId} Creating ${handler} listener`)
+        const removeListener = window.electron.receive('npc:updated', async () => {
+            console.log(`${logId} NPCs updated: reloading ${handler} data`)
+            setCharacters(await window.electron.invoke(`${handler}:get`, data))
+        })
+
+        return () => {
+            if(removeListener) {
+                console.log(`${logId} Removing ${handler} listener`)
+                removeListener()
+            }
+        }
+    }, [data])
+
+    return characters
+}
